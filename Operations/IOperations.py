df04345eac9e267e0d00b47c9c402d004942207d
coder432 <avinashmkamath@gmail.com>
2020-11-16 22:53:06 +0530

    added metaclass

--- Operations/IOperations.py
+++ Operations/IOperations.py
@@ -1,8 +1,11 @@
-class IOperation(object):
+from abc import ABCMeta as ABC,abstractmethod
+class IOperation(metaclass = ABC):
     
+    @abstractmethod
     def operate(self, *parameter_list):
         raise NotImplementedError
 
+    @abstractmethod
     def showOperation(self, *parameter_list):
         raise NotImplementedError
 
--- Operations/add.py
+++ Operations/add.py
@@ -1,5 +1,4 @@
 from Operations.IOperations import IOperation
-
 class add(IOperation):
     
     operationString = '+'
@@ -8,6 +7,8 @@ class add(IOperation):
     def operate(operand1,operand2):
         return operand1 + operand2
 
-    @classmethod
-    def showOperation(cls, operand1, operand2):
-        return str(operand1) + cls.operationString + str(operand2) 
+  
+
+if  __name__ == "__main__":
+    print(add.operate(1,2))
+    
