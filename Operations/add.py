56ce18529178850da40d1051f7dcf35f6baf42a1
coder432 <avinashmkamath@gmail.com>
2020-11-16 22:54:12 +0530

    reverted show op

--- Operations/add.py
+++ Operations/add.py
@@ -7,7 +7,9 @@ class add(IOperation):
     def operate(operand1,operand2):
         return operand1 + operand2
 
-  
+    @classmethod
+    def showOperation(cls, operand1, operand2):
+        return str(operand1) + cls.operationString + str(operand2)
 
 if  __name__ == "__main__":
     print(add.operate(1,2))
